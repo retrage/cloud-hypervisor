0b45a619582c09fd882dfbbaa04e5f2c061f6969
Akira Moroo <retrage01@gmail.com>
2022-01-08 15:54:48 +0900

    build: Fix gdb option arguments
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -379,13 +379,11 @@ fn create_app<'a>(
     );
 
     #[cfg(all(target_arch = "x86_64", feature = "kvm"))]
-    {
-        app = app.arg(
-            Arg::new("gdb")
-                .long("gdb")
-                .help("Enable GDB debugging"),
-        );
-    }
+    let app = app.arg(
+        Arg::new("gdb")
+            .long("gdb")
+            .help("Enable GDB debugging"),
+    );
 
     #[cfg(feature = "tdx")]
     let app = app.arg(
